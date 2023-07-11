70cca550dacc84f787b982fd0e87bf8babf9cc47
KirkDirk <jackalex@ya.ru>
2023-07-11 23:04:01 +0500

    add view and controller

--- src/controllers/NoteController.java
+++ src/controllers/NoteController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import interfaces.DataVerification;
 import interfaces.StorageActions;
+import models.ClsNote;
 
 public class NoteController {
     private StorageActions storageActions;
@@ -15,7 +16,25 @@ public class NoteController {
     // public NoteController(StorageActions storageActions, DataVerification dataVerification) {
     // }
 
+    public ClsNote ParsData(String line){
+        ClsNote note = null;
+        return note;
+    }
     
+    String NoteToLine(ClsNote note){
+        String line = note.getLastName() + " " +
+                        note.getFirstName() + " " +
+                        note.getPatronymic() + " " +
+                        note.getGender() + " " +
+                        note.getBirthDate() + " " +
+                        note.getPhoneNumber() + "\n";
+        return line;
+    }
+
+    public void saveNoteToTxt(ClsNote note){
+        String line = NoteToLine(note);
+        storageActions.SaveNote(line);
+    }
 }
 
 
--- src/views/ViewNote.java
+++ src/views/ViewNote.java
@@ -1,6 +1,9 @@
 package views;
 
+import java.util.Scanner;
+
 import controllers.NoteController;
+import models.ClsNote;
 
 public class ViewNote {
     private NoteController noteController;
@@ -10,6 +13,27 @@ public class ViewNote {
     }
 
     public void run() {
+        boolean continuation = true;
+        do {
+            String dataLine = prompt("Введите данные через пробел: ФИО, пол (m/f), дата рождения (дд.мм.гггг) номер телефона:\n");
+            ClsNote note = noteController.ParsData(dataLine);
+            noteController.saveNoteToTxt(note);
+            if (prompt("Введём ещё данные? y/n:").toUpperCase() == "N") {
+                continuation = false;
+            }            
+        } while (continuation);            
+
     }
     
+    /**
+     * Получение данных с одного ввода с консоли
+     * 
+     * @param message - выводимое сообщение
+     * @return String
+     */
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
 }
